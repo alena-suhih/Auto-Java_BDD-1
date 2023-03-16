b36378b4e0c114bf825c6758b6f07168772b5193
alena-suhih <prhalenka23@gmail.com>
2023-03-16 19:36:31 +0200

    code cleanup

--- src/test/java/ru/netology/data/DataGenerator.java
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -24,10 +24,6 @@ public class DataGenerator {
         return new CardInfo("5559 0000 0000 0002", "0f3f5c2a-249e-4c3d-8287-09f7a039391d");
     }
 
-    public static int generateValidAmount(int balance) {
-        return new Random().nextInt(balance) + 1;
-    }
-
     @Value
     public static class AuthInfo {
         private String login;
